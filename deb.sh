ec8d4fb2a110ad2b64cc518dc81e47edd9012ac8
Dev8904 <jonathanmarshall.IT@pm.me>
2023-11-11 18:37:32 +0100

    kitty configs removal moving to st

--- deb.sh
+++ deb.sh
@@ -30,7 +30,7 @@ bash general.sh
 chown -R $username:$username /home/$username
 
 # Installing Essential Programs 
-apt install curl feh kitty picom thunar nitrogen lxpolkit x11-xserver-utils unzip wget pulseaudio pulseeffects tlp xbacklight pavucontrol variety build-essential libx11-dev libxft-dev libimlib2-dev jq libxinerama-dev xdg-utils libu2f-udev fonts-liberation -y
+apt install curl feh picom thunar nitrogen lxpolkit x11-xserver-utils unzip wget pulseaudio pulseeffects tlp xbacklight pavucontrol variety build-essential libx11-dev libxft-dev libimlib2-dev jq libxinerama-dev xdg-utils libu2f-udev fonts-liberation -y
 # Installing Other less important Programs
 apt install neofetch flameshot psmisc mangohud code lxappearance papirus-icon-theme lxappearance meld mintstick catfish zenity gitfiend tldr fonts-noto-color-emoji flatpak distrobox virt-manager -y
 
